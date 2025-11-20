1d94dfbe70651cf078d8b3abd8a0b876b730d029
Satyanarayana P <satyanarayana.pasumarthy02@gmail.com>
2025-11-20 13:12:58 +0530

    terraform-roboshop-dev-infra

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "cloudtechrnd.space"
+}
+
+variable "zone_id" {
+    default = "Z04238403T15D40S55XVR"
+}
